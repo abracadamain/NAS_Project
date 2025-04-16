06b3fe1189ec4cd782cc684d6b03a842999a6193
abracadamain <paulineigue@gmail.com>
2025-04-16 13:40:15 +0200

    ajout adresse sur le switch des CE

--- gen_adresses.py
+++ gen_adresses.py
@@ -24,30 +24,35 @@ for i in range(len(data["AS"])) :
     for j in range(len(routeurs)) :
         routeur = routeurs[j]
         dict_ip[routeur["hostname"]] = {}
-        if routeur["type"] != "CE" :
+        if routeur["router-type"] != "CE" :
             dict_ip[routeur["hostname"]]["routeur-id"] = indiceRouteurId + "." + indiceRouteurId+ "." +indiceRouteurId+ "." +indiceRouteurId
             indiceRouteurId = str(int(indiceRouteurId) + 1)
-        if routeur["type"] == "PE" :
+        if routeur["router-type"] == "PE" :
             dict_ip[routeur["hostname"]]["loopback"] = loopback[indiceLoopback]
             indiceLoopback += 1
         for k in range(len(routeur["interfaces"])) :
             interface = routeur["interfaces"][k]
-            if interface["voisin"] != "switch" :
-                dict_ip[routeur["hostname"]][interface["name"]] = {}
-                if interface["voisin"] in dict_ip.keys() :
-                    for Vrouteur in routeurs :
-                        if Vrouteur["hostname"] == interface["voisin"] :
-                            for Vinterface in Vrouteur["interfaces"] :
-                                if Vinterface["voisin"] == routeur["hostname"] :
-                                    interfaceVoisin = Vinterface["name"]
-                                    
-                    dict_ip[routeur["hostname"]][interface["name"]]["adresse-reseau"] = dict_ip[interface["voisin"]][interfaceVoisin]["adresse-reseau"]
-                    dict_ip[routeur["hostname"]][interface["name"]]["adresse"] = dict_ip[interface["voisin"]][interfaceVoisin]["adresse"] + 1
-                
-                else :
-                    dict_ip[routeur["hostname"]][interface["name"]]["adresse-reseau"] = subnets[indiceSubnet][0]
-                    dict_ip[routeur["hostname"]][interface["name"]]["adresse"] = subnets[indiceSubnet][1]
-                    indiceSubnet += 1
+            # if interface["voisin"] != "switch" :
+            dict_ip[routeur["hostname"]][interface["name"]] = {}
+            if interface["voisin"] in dict_ip.keys() :
+                for Vrouteur in routeurs :
+                    print("prévoisin", Vrouteur["hostname"], interface["voisin"])
+                    if Vrouteur["hostname"] == interface["voisin"] :
+                        print("voisin", Vrouteur["hostname"])
+                        for Vinterface in Vrouteur["interfaces"] :
+                            print(Vinterface["voisin"], routeur["hostname"], Vinterface["voisin"] == routeur["hostname"])
+                            if Vinterface["voisin"] == routeur["hostname"] :
+                                interfaceVoisin = Vinterface["name"]
+                                print(routeur["hostname"], interface["voisin"], interfaceVoisin, dict_ip[interface["voisin"]][interfaceVoisin]["adresse"])
+
+                                
+                dict_ip[routeur["hostname"]][interface["name"]]["adresse-reseau"] = dict_ip[interface["voisin"]][interfaceVoisin]["adresse-reseau"]
+                dict_ip[routeur["hostname"]][interface["name"]]["adresse"] = dict_ip[interface["voisin"]][interfaceVoisin]["adresse"] + 1
+            
+            else :
+                dict_ip[routeur["hostname"]][interface["name"]]["adresse-reseau"] = subnets[indiceSubnet][0]
+                dict_ip[routeur["hostname"]][interface["name"]]["adresse"] = subnets[indiceSubnet][1]
+                indiceSubnet += 1
 
 with open("adresses.json", "w") as file:
     json.dump(dict_ip, file, indent=4, default=custom_serializer)         
