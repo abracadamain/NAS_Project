5676e7bd046048e7b017eb20c574cb151880845b
zoejingwen <jshen9613@gmail.com>
2025-04-16 16:16:48 +0200

    .

--- Drag_and_Drop.py
+++ Drag_and_Drop.py
@@ -13,20 +13,23 @@ for i in range (len(nodes)):
     node=nodes[i]
     node_id=node["node_id"]
     node_name=node["name"]
+    print(node_name)
     node_info=requests.get(f"http://localhost:3080/v2/projects/{project_id}/nodes/{node_id}").json()
-    node_directory=node_info["node_directory"]
-    file_avant="i"+str(i+1)+"_startup-config.cfg"
-    config_file_path = os.path.join(node_directory, "configs", file_avant)
-    stop_url = f"http://localhost:3080/v2/projects/v2/projects/{project_id}/nodes/{node_id}/stop"
-    requests.post(stop_url)
-    print("Node stopped.")
-    try:
-        
-        file_apres=str(node_name)+".cfg"
-        shutil.copyfile(file_apres,config_file_path)
-        print("Config file replaced successfully.")
-    except Exception as e:
-        print(f"Failed to copy config file: {e}")
-    start_url = f"http://localhost:3080/v2/projects/v2/projects/v2/projects/{project_id}/nodes/{node_id}/start"
-    requests.post(start_url)
-    print("Node started.")
+    if node_info["node_directory"]:
+        node_directory=node_info["node_directory"]
+        file_avant="i"+str(i+1)+"_startup-config.cfg"
+        config_file_path = os.path.join(node_directory, "configs", file_avant)
+        stop_url = f"http://localhost:3080/v2/projects/v2/projects/{project_id}/nodes/{node_id}/stop"
+        requests.post(stop_url)
+        print("Node stopped.")
+        try:
+            
+            file_apres=str(node_name)+".cfg"
+            shutil.copyfile(file_apres,config_file_path)
+            print("Config file replaced successfully.")
+        except Exception as e:
+            print(f"Failed to copy config file: {e}")
+        start_url = f"http://localhost:3080/v2/projects/v2/projects/v2/projects/{project_id}/nodes/{node_id}/start"
+        requests.post(start_url)
+        print("Node started.")
+    
